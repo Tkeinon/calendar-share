3565bfa506b82ffa0552e0d67e1623ab1e598b0e
Tommi <tommi.keinonen@gmail.com>
2025-07-23 19:46:37 +0300

    Make email unique
    Users will be able to login in with both, username and email so emails
    are made to be also unique.

--- backend/calendar_auth/models/user_model.py
+++ backend/calendar_auth/models/user_model.py
@@ -1,5 +1,12 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CalendarUser(AbstractUser):
-    pass
+    email = models.EmailField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.email:
+            self.email = self.email.lower()  # Enforce uniqueness in emails
+        
+        super().save(*args, **kwargs)
