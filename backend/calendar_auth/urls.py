24461b502853e03a36c1447b03c6b8f9ff97c845
Tommi <tommi.keinonen@gmail.com>
2025-07-25 19:52:08 +0300

    Backend: Create login API
    - Custom authentication backend allows login with email and username
    - LoginView handles authentication
    - Tests included

--- backend/calendar_auth/urls.py
+++ backend/calendar_auth/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from calendar_auth.views import views
+from calendar_auth.views import index, login
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    
+    path('', index.index, name='index'),
+    path('api/login/', login.LoginView.as_view(), name='login'),
+
     # Catch-all for React frontend
     path('', TemplateView.as_view(template_name='base.html'), name='react-app'),
 ]
---
+++ backend/calendar_auth/views/login.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+from calendar_auth.views.index import BaseView
+
+
+@method_decorator(csrf_protect, name='dispatch')
+class LoginView(BaseView):
+    def post(self, request, *args, **kwargs):
+        auth_field = request.parsed_data.get('username')
+        password = request.parsed_data.get('password')
+
+        if not auth_field or not password:
+            return JsonResponse({'error': 'Missing credentials'}, status=400)
+
+        user = authenticate(request, username=auth_field, password=password)
+
+        if user:
+            login(request, user)
+
+            return JsonResponse({
+                'message': 'Logged in',
+                'user': user.as_dict(),
+                }, status=200)
+        
+        else:
+            return JsonResponse({'error': 'Missing credentials'}, status=401)
+        
