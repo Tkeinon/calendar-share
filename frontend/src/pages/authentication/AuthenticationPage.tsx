3f396f0f93d443cffc7c7bdb74337501b779f6c2
Tommi <tommi.keinonen@gmail.com>
2025-07-29 20:43:17 +0300

    Make registration possible, frontend
    - App now has simple register form
    - After completed registration, user is redirected to login form

--- frontend/src/pages/authentication/AuthenticationPage.tsx
+++ frontend/src/pages/authentication/AuthenticationPage.tsx
@@ -1,14 +1,25 @@
-import LoginForm from '../../forms/LoginForm';
-import styles from './authentication.module.css';
+import { useState } from 'react';
+import LoginForm from 'src/forms/LoginForm';
+import RegisterForm from 'src/forms/RegisterForm';
+import styles from 'src/pages/authentication/authentication.module.css';
 
 const AuthenticationPage = () => {
+    const [showLoginForm, setShowLoginForm] = useState<boolean>(true);
+
     return <div className={styles.container}>
         <div className={styles['left-panel']}>
             <h1 className={styles.title}>Share Calendar</h1>
             <p className={styles.subtitle}>Plan. Schedule. Enjoy.</p>
         </div>
         <div className={styles['right-panel']}>
-            <LoginForm />
+            {showLoginForm 
+                ? <LoginForm /> 
+                : <RegisterForm onSuccessRegistration={() => setShowLoginForm(true)} />}
+        <div className={styles.footer}>
+                <span onClick={() => setShowLoginForm(!showLoginForm)}>
+                    A new user? Register!
+                </span>
+            </div>
         </div>
     </div>;
 };
